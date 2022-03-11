6aa0d6a5ea5cb38ec3f170174c6df394c6455eaf
laelaps <77897569+lae-laps@users.noreply.github.com>
2022-03-12 00:39:06 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,10 @@
+# Este script instala automáticamente el programa system-wide como utilidad a demás de incluirlo en el ~/.bashrc
+
+if [ "$EUID" -ne 0 ]; then
+    echo "[*] ~ Please run as root"
+    exit
+fi
+
 mv data.py /usr/local/bin/system-data/main.py
 touch /usr/local/bin/system-data
 chmod +x /usr/local/bin/system-data
